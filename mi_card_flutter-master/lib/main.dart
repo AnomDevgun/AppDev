f1ce060d0130bb44eca60e8ca1c896ea9262bea7
anomdvgn <rdevgun7@gmail.com>
2020-05-15 01:37:58 +0530

    changes

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -18,19 +18,52 @@ class myapp extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: Container(
-            height: 200.0,
-            width: 500.0,
-            child: Align(
-              alignment: Alignment.center,
-              child: Text(
-                //containers can only have 1 child
-                'The Witcher 3',
-                style: TextStyle(color: Colors.indigoAccent, fontSize: 25),
+            child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.indigo,
+              height: 100.0,
+              width: 300.0,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  //containers can only have 1 child
+                  'The Witcher 3',
+                  style: TextStyle(color: Colors.pinkAccent),
+                ),
               ),
             ),
-          ),
-        ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              child: Text(
+                'Container 2',
+                textAlign: TextAlign.center,
+              ),
+              color: Colors.blueAccent,
+              width: 300,
+              height: 100,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              child: Text(
+                'Container 3',
+                textAlign: TextAlign.center,
+              ),
+              color: Colors.greenAccent,
+              width: 300,
+              height: 100,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+          ],
+        )),
         floatingActionButton: FloatingActionButton(
           onPressed: null,
           child: Icon(Icons.settings),
