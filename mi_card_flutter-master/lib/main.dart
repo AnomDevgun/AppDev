76a3496f841a6c49134560872f51bdd1f8b7590a
anomdvgn <rdevgun7@gmail.com>
2020-06-10 22:11:55 +0530

    mi-card app done

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -10,9 +10,10 @@ class myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.deepPurpleAccent,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
@@ -33,6 +34,48 @@ class myapp extends StatelessWidget {
                   fontSize: 25.0,
                   color: Colors.black54,
                   letterSpacing: 0.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.deepPurple[900],
+                  thickness: 1.0,
+                ),
+              ),
+              Card(
+                color: Colors.white70,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    '+91 1234567890',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'SansPro',
+                        color: Colors.deepPurple[500]),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white70,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    'flutterdev@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'SansPro',
+                        color: Colors.deepPurple[500]),
+                  ),
                 ),
               ),
             ],
