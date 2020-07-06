be6123a3df69c744abbd0e028dc0dd5ef659a511
anomdvgn <rdevgun7@gmail.com>
2020-07-06 12:45:21 +0530

    dicee being worked on and other changes

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -4,10 +4,11 @@ void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.lightBlue,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text('Dice'),
+          backgroundColor: Colors.blue,
         ),
         body: DicePage(),
       ),
@@ -18,6 +19,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice6.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
