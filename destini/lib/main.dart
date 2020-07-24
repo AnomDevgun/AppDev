6a5281d4b273307f284a6b71fd69de56d63c8101
anomdvgn <rdevgun7@gmail.com>
2020-07-24 20:19:28 +0530

    Completed.

--- destini/lib/main.dart
+++ destini/lib/main.dart
@@ -7,6 +7,7 @@ void main() => runApp(Destini());
 class Destini extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: StoryPage(),
     );
