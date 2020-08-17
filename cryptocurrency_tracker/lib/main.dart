00f6768a03ed08a107f57432839753134fa53c1c
anomdvgn <rdevgun7@gmail.com>
2020-08-17 15:24:23 +0530

    Working on Functionality, implemented Cupertino Style.

--- cryptocurrency_tracker/lib/main.dart
+++ cryptocurrency_tracker/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Cryptocurrency Tracker',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
           primaryColor: Colors.lightBlue,
