5dfbbdff9fc0c8cecaf13c0983a705ea4aaba4f5
anomdvgn <rdevgun7@gmail.com>
2020-08-27 14:06:05 +0530

    Added firebase registration. Upcoming: authentication for login.

--- uchat/lib/main.dart
+++ uchat/lib/main.dart
@@ -3,26 +3,33 @@ import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        ChatScreen.id: (context) => ChatScreen(),
-        LoginScreen.id: (context) => LoginScreen(),
-        RegistrationScreen.id: (context) => RegistrationScreen(),
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-      },
-    );
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData.dark().copyWith(
+                textTheme: TextTheme(
+                  bodyText1: TextStyle(color: Colors.black54),
+                ),
+              ),
+              initialRoute: WelcomeScreen.id,
+              routes: {
+                ChatScreen.id: (context) => ChatScreen(),
+                LoginScreen.id: (context) => LoginScreen(),
+                RegistrationScreen.id: (context) => RegistrationScreen(),
+                WelcomeScreen.id: (context) => WelcomeScreen(),
+              },
+            );
+          }
+        });
   }
 }
--- uchat/lib/screens/registration_screen.dart
+++ uchat/lib/screens/registration_screen.dart
@@ -2,6 +2,8 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:uchat/components/PaddingButtons.dart';
 import 'package:uchat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:uchat/screens/chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const String id = 'registration';
@@ -10,6 +12,9 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance; //Firebase authenticator
+  String email;
+  String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +43,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   height: 48.0,
                 ),
                 TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
                   onChanged: (value) {
-                    //Do something with the user input.
+                    email = value;
                   },
                   decoration: kTextFieldDecoration.copyWith(
                       hintText: 'Enter your email address'),
@@ -48,8 +55,10 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   height: 8.0,
                 ),
                 TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
                   onChanged: (value) {
-                    //Do something with the user input.
+                    password = value;
                   },
                   decoration: kTextFieldDecoration.copyWith(
                       hintText: 'Enter a password'),
@@ -60,9 +69,17 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 PaddingWidget(
                   colour: Colors.blueAccent,
                   title: 'Register',
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.id);
-                    //Go to login screen.
+                  onPressed: () async {
+                    try {
+                      final newUser =
+                          await _auth.createUserWithEmailAndPassword(
+                              email: email, password: password);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
                   },
                 ),
               ],
